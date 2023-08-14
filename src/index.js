823b19e7cdd0cb714f6aed9d126551e1494ef485
Santiago Leon <santiago.leon@binomio.uy>
2023-08-14 19:24:49 -0300

    New pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-import "./index.css";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./store";
-import { App } from "App";
-import { StrictMode } from "react";
+import './index.css'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { StrictMode } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { NoteBrowse } from 'pages/NoteBrowse/NoteBrowse'
+import { Note } from "pages/Note/Note"
+import { NoteCreate } from 'pages/NoteCreate/NoteCreate'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<NoteBrowse />} />
+          <Route 
+            path='/note/:id' 
+            element={<Note />} />
+          <Route path='/note/new' element={<NoteCreate />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
-  </StrictMode>
-);
+  </StrictMode>,
+)
