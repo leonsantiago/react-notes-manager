5e8360541c9c6d91b6964953f198e10943f806ce
Santiago Leon <santiago.leon@fundingcircle.com>
2023-09-09 18:22:18 -0300

    Persist user

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/auth-slice";
 import { noteReducer } from "./notes/notes-slice";
 
-export const store = configureStore({
-  reducer: {
-    notesSlice: noteReducer,
-    authSlice: authReducer,
-  },
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const rootReducer = combineReducers({
+  notesSlice: noteReducer,
+  authSlice: authReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ['authSlice'],
+};
+
+const persistedReducers = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
